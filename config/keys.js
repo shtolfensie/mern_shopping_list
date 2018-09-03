57d1bd7f2c12e713edb3a09c634d2222651f6c26
shtolfensie <31325217+shtolfensie@users.noreply.github.com>
2018-09-03 23:42:16 +0200

    Basic back-end setup for MERN stack app

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://filip:filip123@ds243502.mlab.com:43502/mern_shopping"
+};
---
+++ routes/api/items.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+// Bring in mongo schema
+const Item = require("../../models/Item");
+
+// @route   GET api/items
+// @desc    GET all items
+// @access  public
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then(items => res.json(items));
+});
+
+// @route   POST api/items
+// @desc    POST an item
+// @access  public
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+  newItem.save().then(item => res.json(item));
+});
+
+// @route   DELETE api/items/:id
+// @desc    DELETE an item
+// @access  public
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
