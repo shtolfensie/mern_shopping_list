838c5cdc585ea971c1603d7cf6e49986b1d6e469
shtolfensie <31325217+shtolfensie@users.noreply.github.com>
2018-09-05 18:31:04 +0200

    Implemented adding and removing items in ShoppingList component localy; Added menu header, moved menu items to the right, added github link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,53 @@
-import React, { Component } from 'react'
-import { Menu, Container } from 'semantic-ui-react'
-
+import React, { Component } from "react";
+import { Menu, Container } from "semantic-ui-react";
 
 class Navbar extends Component {
-  state = { activeItem: 'home' }
+  state = { activeItem: "home" };
 
-  handleItemClick = (e, {name}) => {
-    this.setState({activeItem: name});
-  }
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+  };
   render() {
     const { activeItem } = this.state;
     return (
-      <Menu inverted style={{borderRadius: 0}}>
+      <Menu inverted stackable style={{ borderRadius: 0 }}>
         <Container>
-          <Menu.Item name='home' active={ activeItem === 'home'} onClick={this.handleItemClick}>Home</Menu.Item>
-          <Menu.Item name='about' active={ activeItem === 'about'} onClick={this.handleItemClick}>About</Menu.Item>
-          <Menu.Item name='contact' active={ activeItem === 'contact'} onClick={this.handleItemClick}>contact</Menu.Item>
+          <Menu.Item header>ShoppingList</Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item
+              name="home"
+              active={activeItem === "home"}
+              onClick={this.handleItemClick}
+            >
+              Home
+            </Menu.Item>
+            <Menu.Item
+              name="about"
+              active={activeItem === "about"}
+              onClick={this.handleItemClick}
+            >
+              About
+            </Menu.Item>
+            <Menu.Item
+              name="contact"
+              active={activeItem === "contact"}
+              onClick={this.handleItemClick}
+            >
+              Contact
+            </Menu.Item>
+            <Menu.Item
+              name="github"
+              // active={activeItem === "github"}
+              // onClick={this.handleItemClick}
+              href="https://github.com/shtolfensie"
+              target="_blank"
+            >
+              Github
+            </Menu.Item>
+          </Menu.Menu>
         </Container>
       </Menu>
-    )
+    );
   }
 }
 
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,38 +1,63 @@
-import React, { Component } from 'react'
-import { Container, Button, List } from 'semantic-ui-react'
-import uuid from 'uuid'
-
+import React, { Component } from "react";
+import { Container, Button, List, Header } from "semantic-ui-react";
+import uuid from "uuid";
 
 class ShoppingList extends Component {
-
   state = {
     items: [
-      { name: 'Milk', id: uuid() },
-      { name: 'Toast', id: uuid() },
-      { name: 'Gatorade', id: uuid() },
-      { name: 'Aligator', id: uuid() }
+      { name: "Milk", id: uuid() },
+      { name: "Toast", id: uuid() },
+      { name: "Gatorade", id: uuid() },
+      { name: "Aligator", id: uuid() }
     ]
-  }
+  };
 
   render() {
     const { items } = this.state;
     return (
       <Container>
-        <Button content='Add item'/>
+        <Button
+          content="Add item"
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name)
+              this.setState(state => ({
+                items: [...state.items, { name, id: uuid() }]
+              }));
+          }}
+        />
         <List divided>
           {items.map(item => (
-            <List.Item key={item.id}  >
-              <List.Content style={{display: 'flex', justifyContent: 'space-between'}}>
-                <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>{item.name}</div>
-                <Button content='Remove' />
+            <List.Item key={item.id}>
+              <List.Content
+                style={{ display: "flex", justifyContent: "space-between" }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column"
+                  }}
+                >
+                  <Header size="small">{item.name}</Header>
+                </div>
+                <Button
+                  content="Remove"
+                  onClick={() => {
+                    this.setState(state => ({
+                      items: state.items.filter(
+                        currItem => currItem.id !== item.id
+                      )
+                    }));
+                  }}
+                />
               </List.Content>
             </List.Item>
           ))}
         </List>
       </Container>
-    )
+    );
   }
-
 }
 
 export default ShoppingList;
